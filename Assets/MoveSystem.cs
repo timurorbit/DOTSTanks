707520edcb8de82e2924250c698f7a5fe73c847d
Timur <t.shamilov.hollow@gmail.com>
2024-11-07 09:17:49 +0100

    Initial commit

---
+++ Assets/MoveSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class MoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities
+               .WithName("MoveSystem")
+               .ForEach((ref Translation position) =>
+               {
+                   position.Value.z += 0.05f;
+               })
+               .Schedule(inputDeps);
+
+        return jobHandle;
+    }
+
+}
